f75917f1d7ca26a06d6b502890d6e6c7125abee6
ziaulsarker <ziaulsarker@gmail.com>
2019-04-08 03:04:08 -0400

    added meta tags

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 
 router.get("/", (req, res, next) => {
-    res.render("index", {title: "Ziaul | Home"});
+    res.render("index", {title: "Ziaul Sarker | Front End Developer"});
 });
 
 
